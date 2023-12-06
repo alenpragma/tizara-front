3972461c657bf7d18561302607827d7917fa4c01
Remon Tripura <remontripura045@gmail.com>
2023-12-06 12:57:41 +0600

    text upgrade

--- app/page.js
+++ app/page.js
@@ -57,7 +57,7 @@ const page = () => {
               <div className="flex flex-col justify-center items-center md:justify-start md:items-start">
                 <h2 className=" text-[35px] lg:text-[45px] mb-0">24/7</h2>
                 <p className="text-grey font-semibold mt-[-5px]">
-                  Supported
+                  Service
                 </p>
               </div>
 
@@ -90,7 +90,7 @@ const page = () => {
             <div className="flex justify-between w-full md:w-[20%]  md:flex-col md:gap-y-10">
               <div className="flex flex-col justify-center items-center md:justify-start md:items-end">
                 <div className="flex flex-col items-center md:items-start">
-                <h2 className=" text-[35px] lg:text-[45px] mb-0">24/78</h2>
+                <h2 className=" text-[35px] lg:text-[45px] mb-0">24/7</h2>
                 <p className="text-grey font-semibold mt-[-5px]">
                 Customer Support
                 </p>

c2de5228cfa6f42f323643e614613146a4fff01b
remontripura <remontripura045@gmail.com>
2025-08-13 11:34:30 +0600

    update wallet address

--- app/_components/_tizara-coin/TizaraCoin.js
+++ app/_components/_tizara-coin/TizaraCoin.js
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { FaRegCopy } from "react-icons/fa";
 
 const TizaraCoin = () => {
-    const token = "0x0a30F4BC6D57bA1Be0f1ac117de97E6D4889420d"
+    const token = "0xA5329958575BE2eeb6F5bcA5F1af69d9A407E53d"
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(token)
@@ -64,7 +64,7 @@ const TizaraCoin = () => {
                 <div className=" border-l border-[#07f9c873]  col-span-6 ">
                     <div className="w-[99%] h-[98%] m-auto rounded shadow-2xl bg-[#07f9c81c] py-10 flex flex-col  pl-5 justify-center  duration-300 hover:translate-y-[4%]">
                         <h3 className='text-[16px] text-[#08CBA4]'>Contract Address</h3>
-                        <p className="md:text-[16px] text-[10px]  duration-300 flex items-center gap-3" >0x0a30F4BC6D57bA1Be0f1ac117de97E6D4889420d <span>
+                        <p className="md:text-[16px] text-[10px]  duration-300 flex items-center gap-3">{token}<span>
                             <FaRegCopy className='text-[18px] hover:text-slate-400 cursor-pointer' onClick={copyToClipboard} />
                         </span></p>
                     </div>
